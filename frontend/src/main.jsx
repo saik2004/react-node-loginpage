d2a374c9340fcc25a9f9b3ca822f615b1235702f
saik2004 <saisk04117@gmail.com>
2026-09-23 17:13:58 +0530

    connect frontend to deployed backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Dashboard from './components/dashboard.jsx'
+import Dashboard from './components/Dashboard.jsx'
 import Loginfailed from './components/Loginfailed.jsx'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
