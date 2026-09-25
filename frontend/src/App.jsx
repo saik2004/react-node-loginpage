4fa4770722fd651eec7012b1a5f791e131c083f2
saik2004 <saisk04117@gmail.com>
2026-09-25 10:31:51 +0530

    updates demo credentials

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       
       <div className='flex justify-center mt-15'>
         <div className='flex flex-col rounded-sm  gap-1 p-3 text-xs items-center w-fit  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]'>
-          <p>For user-experience try</p>
+          <p>Demo credentials</p>
           <p>Email:test123@gmail.com</p>
           <p>Password:abc123</p>
         </div>
